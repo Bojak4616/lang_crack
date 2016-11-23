e4661554f985e7b9a261d99f461fb6e807340559
Jesse Buonanno <jpb4616@rit.edu>
2016-11-22 21:17:48 -0500

    Changed argument wording

--- lang_crack.py
+++ lang_crack.py
@@ -5,11 +5,12 @@ import hashlib
 
 __author__ = "Jesse Buonanno"
 
-if __name__ == '__main__':
+
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", help="Specify Verbose", action="store_true")
     parser.add_argument("hash_type", help="Supported hash types are MD5, SHA1, SHA256", type=str)
-    parser.add_argument("attack_type", help="Supported hash types are bruteforce(bf) or wordlist(wl)",
+    parser.add_argument("attack_type", help="Supported hash types are bruteforce(bf) or wordlist(wl). If wl, then specify file.",
                         choices=["bf", "wl"], type=str)
     args = parser.parse_known_args()
 
@@ -19,7 +20,13 @@ if __name__ == '__main__':
 
     parser.add_argument("hash_file", help="File containing hashed passwords", type=str)
     parser.add_argument('--output', type=argparse.FileType('wb', 0), default="output.txt")
-    #TODO: Add threading and threading argument. Depends how large the password files will be
-    args = parser.parse_args()
+    # TODO: Add threading and threading argument. Depends how large the password files will be
+    return parser.parse_args()
+
+if __name__ == '__main__':
+
+    args = parse_args()
+    print args
+
 
 
